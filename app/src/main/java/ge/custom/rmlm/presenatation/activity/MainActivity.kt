b4a7581040e8bbea1dfe264e13ee1be940e5defc
fnrngg <balsh17@freeuni.edu.ge>
2025-08-01 02:21:40 +0400

    Enabled edgeToEdge

--- app/src/main/java/ge/custom/rmlm/presenatation/activity/MainActivity.kt
+++ app/src/main/java/ge/custom/rmlm/presenatation/activity/MainActivity.kt
@@ -1,13 +1,18 @@
 package ge.custom.rmlm.presenatation.activity
+
 import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -42,7 +47,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
             MainScreen()
         }
@@ -54,7 +59,10 @@ private fun MainScreen() {
     RMLMTheme {
         val navController = rememberNavController()
         Scaffold(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .fillMaxSize()
+                .safeContentPadding(),
             bottomBar = {
                 val backStackEntry by navController.currentBackStackEntryAsState()
                 val currentDestination = backStackEntry?.destination
